8dc4afef05c845afa1cad0781f70c39a76148858
Sam Bossley <bossley.samuel@gmail.com>
2022-02-17 16:05:08 -0800

    Add dvd logo

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
-int main() { printf("Hello world!\n"); }
+void print_dvd() {
+  printf(" ▀▀▀█   █▀▀▀▄\n");
+  printf(" █  ██▄█ █  █\n");
+  printf(" ▀▀▀▄▄█▄▄▀▀▀\n");
+  printf("▄███ █▀▀████▄\n");
+  printf(" ▀▀▀█████▀▀▀\n");
+}
+
+int main() { 
+  print_dvd();
+}
